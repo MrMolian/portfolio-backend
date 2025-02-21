3acc294027ff7fe37ea1dfa7012c9a373a600b1a
MrMolian <99142979+MrMolian@users.noreply.github.com>
2025-02-21 15:04:01 +0100

    Update urls.py

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,5 @@ from rest_framework.routers import DefaultRouter
 from . import views
 urlpatterns = [
      path('projets/', views.ProjectAPIView.as_view(), name='projet-list'),
+     path('message/', views.MessageAPIView.as_view(), name='message-list'),
 ]
