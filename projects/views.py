62616d32b2f77133f34cbe360ccf3de22cca7cba
MrMolian <99142979+MrMolian@users.noreply.github.com>
2025-02-21 15:04:17 +0100

    Update views.py

--- projects/views.py
+++ projects/views.py
@@ -2,9 +2,9 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.viewsets import ViewSet
-from .models import Project
+from .models import Project,YearMessage
 from rest_framework import filters
-from .serializers import ProjectSerializer
+from .serializers import ProjectSerializer,MessageSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 
 class ProjectAPIView(generics.ListAPIView):
@@ -14,3 +14,11 @@ class ProjectAPIView(generics.ListAPIView):
                            filters.SearchFilter,
 			 filters.OrderingFilter]
         search_fields = ['ladate']
+
+class MessageAPIView(generics.ListAPIView):
+        queryset = YearMessage.objects.all()
+        serializer_class = MessageSerializer
+        filter_backends = [DjangoFilterBackend,
+                           filters.SearchFilter,
+			 filters.OrderingFilter]
+        search_fields = ['year']
