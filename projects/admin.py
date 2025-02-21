8919ed23cb170ce6528ef9878cbaaf3fe11bdb3e
MrMolian <99142979+MrMolian@users.noreply.github.com>
2025-02-21 15:15:29 +0100

    Update admin.py

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from projects.models import Project
+from projects.models import Project,YearMessage
 
 admin.site.register(Project)
 admin.site.register(YearMessage)
